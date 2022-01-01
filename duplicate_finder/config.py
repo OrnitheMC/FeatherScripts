5647e6277db9041ea56bbf1d4632cd3c43c13fc7
Viktor VN <vannieuwenhuize.viktor@gmail.com>
2022-01-01 12:14:05 +0100

    now uses the Pathlib introduced in python 3.4 instead of os

---
+++ duplicate_finder/config.py
@@ -0,0 +1,6 @@
+from typing_extensions import Final
+from pathlib import Path
+
+"""---- Config ----"""
+MAPPING_FOLDER: Final[Path] = Path("../mappings")  # the path to the mappings folder
+DEBUG: Final[bool] = True
--- duplicate_finder/duplicate_classes_finder.py
+++ duplicate_finder/duplicate_classes_finder.py
@@ -1,11 +1,7 @@
-from os import listdir
-from typing import List
+from pathlib import Path
 from collections import defaultdict
-from os.path import isfile, join
 
-"""---- Config ----"""
-path = "..\\mappings"  # the path to the mappings folder
-mappings = defaultdict(list)  # the dictionary to put the mappings in
+from config import MAPPING_FOLDER, DEBUG
 
 
 def main():
@@ -13,50 +9,36 @@ def main():
     Main function checking for duplicates inside the mappings folder. If duplicates
     are found an error is raised and the duplicates are printed.
     """
-    dirs = unpack_directory(path)
-    travel_directory(dirs)
-    duplicates = dict(filter(lambda elem: len(elem[1]) > 1, mappings.items()))
+    mappings = defaultdict(list)
+    for f in MAPPING_FOLDER.glob('**/*'):
+        if f.is_file():
+            add_to_dict(f, mappings)
+
+    duplicates = find_duplicates(mappings)
     if duplicates:
         raise DuplicateMappings(len(duplicates), duplicates)
 
 
-def travel_directory(dirs: list):
-    """
-    This function travels through a directory. If the entry is a file it will add it to the
-    dictionary containing the mappings. If the entry is a dictionary it will again call
-    this method on that dictionary.
-    :param dirs: a list of folders and files that are in the directory
-    """
-    for f in dirs:
-        if isfile(f):
-            add_to_dict(f)
-        else:
-            travel_directory(unpack_directory(f))
-
-
-def unpack_directory(dir_path: str) -> List[str]:
-    """
-    This function unpacks a directory into it's components (files and subdirectories)
-    :param dir_path: a path to a directory
-    :return: a list of paths to the files and directories inside the directory
-    """
-    return [join(dir_path, f) for f in listdir(dir_path)]
-
-
-def add_to_dict(file: str):
+def add_to_dict(file: Path, mapping_dict: defaultdict):
     """
     This method reads a file and checks the mapped class. It will add the file to the
     directory containing all of the mappings. The key is the class name in the intermediaries.
     The value is a list of all the different names this class is mapped under. Each element
     in this list is a list it self containing the mapped name of the entry and the file path.
-    :param file: the file to be analyzed
     """
     with open(file) as f:
-        class_mapping = f.readline().split(' ')[1:]
+        class_mapping = f.readline().strip('\n').split(' ')[1:]
 
-    class_ = class_mapping[0].strip('\n')
-    mapping = class_mapping[1].strip('\n') if len(class_mapping) == 2 else class_
-    mappings[class_].append([mapping, file])
+    class_ = class_mapping[0]
+    mapping = class_mapping[1] if len(class_mapping) == 2 else class_
+    mapping_dict[class_].append([mapping, file])
+
+
+def find_duplicates(mapping_dict: defaultdict) -> dict:
+    """
+    Filter the dict to find the entries with more than 1 name
+    """
+    return dict(filter(lambda elem: len(elem[1]) > 1, mapping_dict.items()))
 
 
 class DuplicateMappings(Exception):
